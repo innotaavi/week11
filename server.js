1f7ea88a2a80550bb2b7597186df12b27917de6d
innotaavi <taavi.inno@tktk.ee>
2026-05-16 20:58:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const fullRecipesRouter = require('./routes/fullrecipes.routes');
 const randomRouter = require('./routes/randomrecipe.routes');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/ingredients', ingredientRouter);
 app.use('/recipes', recipeRouter);
 app.use('/fullRecipes', fullRecipesRouter);
